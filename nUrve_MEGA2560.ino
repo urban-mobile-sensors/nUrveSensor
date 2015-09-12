7ae65ad82fc03388bcc892bdfef395514d1f82a3
Nadir Ait-Laoussine <nadirait@users.noreply.github.com>
2015-09-12 17:11:56 -0400

    Update nUrve_MEGA2560.ino
    Continued to update the GPS Print out, and overall debugging

--- nUrve_MEGA2560.ino
+++ nUrve_MEGA2560.ino
@@ -18,6 +18,10 @@ int IterationCounter = 0;
 const int chipSelect = 10; // For SD Data Logger
 
 // GPS vars
+#define GPSECHO false
+boolean usingInterrupt = false;
+void useInterrupt(boolean); // Func prototype keeps Arduino 0023 happy
+Adafruit_GPS GPS(&Serial1);
 
 //  Sensor vars
 Adafruit_HTU21DF htu = Adafruit_HTU21DF();                                            //HTUD21DF I2C Address: 0x40 (cannot be changed)
@@ -26,24 +30,12 @@ Adafruit_TSL2561_Unified  tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 123
 // Accelerometer vars
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);                     // ADXL345 I2C Address: ????
 
-// === CLEAN UP BELOW ===
-// For GPS
-SoftwareSerial mySerial(3, 2);
-Adafruit_GPS GPS(&mySerial);
-
-
-#define GPSECHO false       // Do we need this for production?
-boolean usingInterrupt = false;   // Do we need this for production?
-void useInterrupt(boolean);     // Func prototype keeps Arduino 0023 happy. Do we need this for production?
-
-// === CLEAN UP ABOVE ===
-
 // Define all the variables that we're going to need:
+String GPS_Date = "0000-00-00";
+String GPS_Time = "HH:MM:SS.MS";
 float GPS_Lat = -9999;
 float GPS_Lon = -9999;
-float GPS_Time = -9999;
-float GPS_Date = -9999;
-float GPS_Alt = -9999;
+float GPS_Altitude = -9999;
 float GPS_Speed = -9999;
 float GPS_Angle = -9999;
 float GPS_Sats = -9999;
@@ -57,7 +49,6 @@ float RDQ_AcZ = -9999;
 
 const int AMB_SND_sampleWindow = 500; // Sample window width in mS (50 mS = 20Hz)
 unsigned int AMB_SND_sample;
-uint32_t timer = millis(); // PART OF GPS
 
 void setup() {
   // initialize serial and wait for the port to open:
@@ -77,6 +68,39 @@ void setup() {
   setupRoadSensor();        // Set up Road Sensor
 }
 
+
+// ADDITIONAL CODE
+// GPS
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+#ifdef UDR0
+  if (GPSECHO)
+    if (c) UDR0 = c;  
+    // writing direct to UDR0 is much much faster than Serial.print 
+    // but only one character can be written at a time. 
+#endif
+}
+
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  } else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
+  }
+}
+
+uint32_t timer = millis();
+// END OF GPS
+// END OF ADDITIONAL CODE
+
 void loop() {
   IterationCounter ++;
   Serial.print("+-------------------------------------------------------------------------+\n");
@@ -84,6 +108,12 @@ void loop() {
   Serial.print("+-------------------------------------------------------------------------+\n");
   Serial.print("| Iteration: #");Serial.println(IterationCounter);
   
+  Serial.print("+-------------------------------------------------------------------------+\n");
+  Serial.print("| Communication Data: [TO DO]\n");
+  Serial.print("| SSID: ");Serial.println("");
+  Serial.print("| RSSI: ");Serial.println("");
+  Serial.print("| Nets: ");Serial.println("");
+
   // GPS CODE
   if (! usingInterrupt) {
     // read data from the GPS in the 'main loop'
@@ -105,14 +135,31 @@ void loop() {
   if (millis() - timer > 2000) { 
     timer = millis(); // reset the timer
     if (GPS.fix) {
-      Serial.print(GPS.latitudeDegrees, 6);
-      Serial.println(GPS.longitudeDegrees, 6);
-      Serial.println(GPS.speed);
-      Serial.println(GPS.angle);
-      Serial.println(GPS.altitude);
-      Serial.println((int)GPS.satellites);
+//      GPS_Date = Serial.println(GPS.year);
+//      GPS_Time = "HH:MM:SS.MS";
+      GPS_Lat = GPS.latitudeDegrees;
+      GPS_Lon = GPS.longitudeDegrees;
+      GPS_Speed = GPS.speed;
+      GPS_Angle = GPS.angle;
+      GPS_Altitude = GPS.altitude;
+      GPS_Sats = GPS.satellites;
     }
   }
+  // STILL TO DO HERE:
+  // 1) Get Date as a single properly formatted string YYYY-MM-DD
+  // 2) Get Time as a single properly formatted string HH:MM:SS.mmm
+  Serial.print("+-------------------------------------------------------------------------+\n");
+  Serial.print("| GPS Information:\n");
+  Serial.print("| Date: ");Serial.println(GPS_Date);
+  Serial.print("20");Serial.print(GPS.year, DEC);Serial.print(":");Serial.print(GPS.month, DEC);Serial.print(":");Serial.print(GPS.month, DEC);Serial.print(":");Serial.println(GPS.day, DEC);
+  Serial.print("| Time: ");Serial.println(GPS_Time);
+  Serial.print(GPS.hour, DEC); Serial.print(':');Serial.print(GPS.minute, DEC); Serial.print(':');Serial.print(GPS.seconds, DEC); Serial.print('.');Serial.println(GPS.milliseconds);
+  Serial.print("| Lat: ");Serial.println(GPS_Lat,6);
+  Serial.print("| Lon: ");Serial.println(GPS_Lon,6);
+  Serial.print("| Speed: ");Serial.println(GPS_Speed,2);
+  Serial.print("| Altitude: ");Serial.println(GPS_Altitude,2);
+  Serial.print("| Angle: ");Serial.println(GPS_Angle,2);
+  Serial.print("| Sats: ");Serial.println(GPS_Sats,0);
   // END OF GPS CODE
 
   // =============================================================================================

4f3a46f09f6c577b765ff43e5b6554712fc5c3e0
Nadir Ait-Laoussine <nadirait@users.noreply.github.com>
2015-10-08 22:45:21 -0400

    Updated for DataFile Issue
    Creates 2 Data Files. Master and Temp.

--- SensorSetup.ino
+++ SensorSetup.ino
@@ -9,9 +9,6 @@
  * 7) Air Quality Sensor - Particulates
  * 8) Other / Future
  */
-void setupComms() {
-//  Serial.println("LATER: Comms");
-}
 
 /* +-------------------------------------------------------------------------+
  * | DATA LOGGER                                                             |
@@ -36,12 +33,19 @@ void setupDataLogger() {
   // 3) Check to see if the data file is here is there.
   // If not, create it and add header row...
   // Keep data data file open
-  if (!SD.exists("datalog.txt")) {
+  if (!SD.exists("m_log.csv")) {
     // Create file
-    dataFile = SD.open("datalog.txt", FILE_WRITE);
+    dataFile_Master = SD.open("m_log.csv", FILE_WRITE);
     // Add header row
-    dataFile.println("ID,DATESTAMP,TIMESTAMP,GPS_LAT,GPS_LON,GPS_Speed,GPS_SpeedMin,GPS_SpeedMax,GPS_SpeedMea,GPS_SpeedMed,GPS_Alt,GPS_Sats,GPS_Fix,GPS_Quality,AMB_Temp,AMB_TempMin,AMB_TempMax,AMB_TempMea,AMB_TempMed,AMB_Humd,AMB_HumdMin,AMB_HumdMax,AMB_HumdMea,AMB_HumdMed,AMB_Lux,AMB_LuxMin,AMB_LuxMax,AMB_LuxMea,AMB_LuxMed,AMB_Snd,AMB_SndMin,AMB_SndMax,AMB_SndMea,AMB_SndMed,RDQ_AcX,RDQ_AcXMin,RDQ_AcXMax,RDQ_AcXMea,RDQ_AcXMed,RDQ_AcY,RDQ_AcYMin,RDQ_AcYMax,RDQ_AcYMea,RDQ_AcYMed,RDQ_AcZ,RDQ_AcZMin,RDQ_AcZMax,RDQ_AcZMea,RDQ_AcZMed");
+    dataFile_Master.println("ID,DATESTAMP,TIMESTAMP,GPS_LAT,GPS_LON,GPS_Speed,GPS_SpeedMin,GPS_SpeedMax,GPS_SpeedMea,GPS_SpeedMed,GPS_Alt,GPS_Sats,GPS_Fix,GPS_Quality,AMB_Temp,AMB_TempMin,AMB_TempMax,AMB_TempMea,AMB_TempMed,AMB_Humd,AMB_HumdMin,AMB_HumdMax,AMB_HumdMea,AMB_HumdMed,AMB_Lux,AMB_LuxMin,AMB_LuxMax,AMB_LuxMea,AMB_LuxMed,AMB_Snd,AMB_SndMin,AMB_SndMax,AMB_SndMea,AMB_SndMed,RDQ_AcX,RDQ_AcXMin,RDQ_AcXMax,RDQ_AcXMea,RDQ_AcXMed,RDQ_AcY,RDQ_AcYMin,RDQ_AcYMax,RDQ_AcYMea,RDQ_AcYMed,RDQ_AcZ,RDQ_AcZMin,RDQ_AcZMax,RDQ_AcZMea,RDQ_AcZMed");
   }
+  if (!SD.exists("t_log.csv")) {
+    // Create file
+    dataFile_Temp = SD.open("t_log.csv", FILE_WRITE);
+    // Add header row
+    dataFile_Temp.println("ID,DATESTAMP,TIMESTAMP,GPS_LAT,GPS_LON,GPS_Speed,GPS_SpeedMin,GPS_SpeedMax,GPS_SpeedMea,GPS_SpeedMed,GPS_Alt,GPS_Sats,GPS_Fix,GPS_Quality,AMB_Temp,AMB_TempMin,AMB_TempMax,AMB_TempMea,AMB_TempMed,AMB_Humd,AMB_HumdMin,AMB_HumdMax,AMB_HumdMea,AMB_HumdMed,AMB_Lux,AMB_LuxMin,AMB_LuxMax,AMB_LuxMea,AMB_LuxMed,AMB_Snd,AMB_SndMin,AMB_SndMax,AMB_SndMea,AMB_SndMed,RDQ_AcX,RDQ_AcXMin,RDQ_AcXMax,RDQ_AcXMea,RDQ_AcXMed,RDQ_AcY,RDQ_AcYMin,RDQ_AcYMax,RDQ_AcYMea,RDQ_AcYMed,RDQ_AcZ,RDQ_AcZMin,RDQ_AcZMax,RDQ_AcZMea,RDQ_AcZMed");
+  }
+
 }
 
 void setupGPS() {
@@ -103,8 +107,71 @@ void setupRoadSensor() {
     Serial.println("[] ERROR: Could not find the ADXL345. Please check your wiring!");
  //   while(1);
   } else {
-    accel.setRange(ADXL345_RANGE_16_G);
+    accel.setRange(ADXL345_RANGE_8_G);
     Serial.println("| ADXL345: All Clear! - ADXL345 found and calibrated.");
   }
   Serial.print("+-------------------------------------------------------------------------+\n");
 }
+
+
+// FONA SETUP - IN HEAVY DEV
+//void setupComms() {
+//  pinMode(FONA_PS, INPUT);
+//  pinMode(FONA_KEY,OUTPUT);   
+//  digitalWrite(FONA_KEY, HIGH);
+//  Serial.println("Started setup");
+//  fonaSS.begin(9600);
+//    /*=============Get Location and Post ONCE on Boot=============*/
+//    /*
+//    turnOnFONA(); //turn on FONA
+//    Serial.println("Initializing: please wait 10 sec...");
+//    delay(10000);
+//    setupGPRS(); //Setup a GPRS context
+//    if(getLocation()) { //try to get location, if OK do the rest
+//        //Print Lat/Lon Values
+//        Serial.print(Lat);
+//        Serial.print(" : ");
+//        Serial.print(Lon);
+//        Serial.print(" at ");
+//        Serial.print(Time);
+//        Serial.print(" ");
+//        Serial.print(Date);
+//        Serial.println();
+//        //Move data into data array. (note: this is an extra step, but helps the legibility of what is going on)
+//        fieldData[0] = Lat; 
+//        fieldData[1] = Lon;
+//        fieldData[2] = Date;
+//        fieldData[3] = Time;
+//    }
+//    makeRequest(); //Make GET request
+//    turnOffFONA(); //turn off FONA
+//    flushFONA();
+//    */
+//    Serial.println("Setup DONE!");
+//}
+//
+//boolean sendATCommand(char Command[]) { //Send an AT command and wait for a response
+//    int complete = 0; // have we collected the whole response?
+//    char c; //capture serial stream
+//    String content; //place to save serial stream
+//    unsigned long commandClock = millis(); //timeout Clock
+//    fonaSS.println(Command); //Print Command
+//    while(!complete && commandClock <= millis() + ATtimeOut) { //wait until the command is complete
+//        while(!fonaSS.available() && commandClock <= millis()+ATtimeOut); //wait until the Serial Port is opened
+//        while(fonaSS.available()) { //Collect the response
+//            c = fonaSS.read(); //capture it
+//            if(c == 0x0A || c == 0x0D); //disregard all new lines and carrige returns (makes the String matching eaiser to do)
+//            else content.concat(c); //concatonate the stream into a String
+//        }
+//        //Serial.println(content); //Debug
+//        response = content; //Save it out to a global Variable (How do you return a String from a Function?)
+//        complete = 1;  //Lable as Done.
+//    }
+//    if (complete ==1) return 1; //Is it done? return a 1
+//    else return 0; //otherwise don't (this will trigger if the command times out) 
+//    /*
+//        Note: This function may not work perfectly...but it works pretty well. I'm not totally sure how well the timeout function works. It'll be worth testing. 
+//        Another bug is that if you send a command that returns with two responses, an OK, and then something else, it will ignore the something else and just say DONE as soon as the first response happens. 
+//        For example, HTTPACTION=0, returns with an OK when it's intiialized, then a second response when the action is complete. OR HTTPREAD does the same. That is poorly handled here, hence all the delays up above. 
+//    */
+//}
